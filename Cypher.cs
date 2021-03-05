7e85428c7acaf9223b46f2f6fd4ae1f5cc310fd9
AlexMayson <80123095+AlexMayson@users.noreply.github.com>
2021-03-06 01:14:46 +0700

    Add files via upload

---
+++ Cypher.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shifrovalshik
+{
+    class Cypher
+    {
+        static string[,] Rus = {{"Аа", "Бб", "Вв", "Гг", "Дд"},
+                                  {"Ее", "Жж", "Зз", "Ии", "Йй"},
+                                  {"Кк", "Лл", "Мм", "Нн", "Оо"},
+                                  {"Пп", "Рр", "Сс", "Тт", "Уу"},
+                                  {"Фф", "Хх", "Цц", "Чч", "Шш"},
+                                  {"Щщ", "Ъъ", "Ыы", "Ьь", "Ээ"},
+                                  {"Юю", "Яя", "!!", ",,", ".."}};
+
+        static string[,] Lat = {{"Aa", "Bb", "Cc", "Dd", "Ee"},
+                                  {"Ff", "Gg", "Hh", "Ii", "Jj"},
+                                  {"Kk", "Ll", "Mm", "Nn", "Oo"},
+                                  {"Pp", "Qq", "Rr", "Ss", "Tt"},
+                                  {"Uu", "Vv", "Ww", "Xx", "Yy"},
+                                  {"Zz", "..", ",,", "!!", "??"}};
+
+        static Dictionary<string, string[,]> alphabets = new Dictionary<string, string[,]>() {
+            {"Russian", Rus},
+            {"Latin", Lat }
+        };
+
+        //Проверка принадлежности текста к определенному алфавиту
+        static bool isValid(string text, string[,] alphabet)
+        {
+            bool[] temp = new bool[text.Length];
+
+            for (int a = 0; a < text.Length; a++)
+            {
+                for (int i = 0; i < alphabet.GetLength(0); i++)
+                {
+                    for (int j = 0; j < alphabet.GetLength(1); j++)
+                    {
+                        if (text[a] == alphabet[i, j][0] || text[a] == alphabet[i, j][1])
+                        {
+                            temp[a] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //Получение названия алфавита введенного текста
+        static string GetAlphabetName(string text)
+        {
+            foreach (KeyValuePair<string, string[,]> kvp in alphabets)
+            {
+                string key = kvp.Key;
+                if (isValid(text, kvp.Value))
+                {
+                    return key;
+                }
+            }
+
+            return null;
+        }
+
+        //Кодирование текста на основе определенного алфавита
+        static string Encode(string text, string[,] alphabet)
+        {
+            string buffer = "";
+
+            for (int a = 0; a < text.Length; a++)
+            {
+                for (int i = 0; i < alphabet.GetLength(0); i++)
+                {
+                    for (int j = 0; j < alphabet.GetLength(1); j++)
+                    {
+                        if (text[a] == alphabet[i, j][0])
+                        {
+                            if (i + 1 < alphabet.GetLength(0))
+                                buffer += alphabet[i + 1, j][0];
+                            else
+                                buffer += alphabet[0, j][0];
+                        }
+                        else if (text[a] == alphabet[i, j][1])
+                        {
+                            if (i + 1 < alphabet.GetLength(0))
+                                buffer += alphabet[i + 1, j][1];
+                            else
+                                buffer += alphabet[0, j][1];
+                        }
+                    }
+                }
+            }
+
+            return buffer;
+        }
+
+
+        //Декодирование текста
+        static string Decode(string text, string[,] alphabet)
+        {
+            string buffer = "";
+
+            for (int a = 0; a < text.Length; a++)
+            {
+                for (int i = 0; i < alphabet.GetLength(0); i++)
+                {
+                    for (int j = 0; j < alphabet.GetLength(1); j++)
+                    {
+                        if (text[a] == alphabet[i, j][0])
+                        {
+                            if (i - 1 >= 0)
+                                buffer += alphabet[i - 1, j][0];
+                            else
+                                buffer += alphabet[alphabet.GetLength(0) - 1, j][0];
+                        }
+                        else if (text[a] == alphabet[i, j][1])
+                        {
+                            if (i - 1 >= 0)
+                                buffer += alphabet[i - 1, j][1];
+                            else
+                                buffer += alphabet[alphabet.GetLength(0) - 1 , j][1];
+                        }
+                    }
+                }
+            }
+
+            return buffer;
+        }
+
+        //Кодирование шифром Полибия
+        public static string EncodePolibiy(string slovo)
+        {
+            string alphabetName = GetAlphabetName(slovo);
+            if (alphabetName != null)
+            {
+                return Encode(slovo, alphabets[alphabetName]);
+            }
+
+            return null;
+        }
+
+        //Декодирование шифром Полибия
+        public static string DecodePolibiy(string slovo)
+        {
+            string alphabetName = GetAlphabetName(slovo);
+            if (alphabetName != null)
+            {
+                return Decode(slovo, alphabets[alphabetName]);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Shifrovalshik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputData = new System.Windows.Forms.RichTextBox();
+            this.outputData = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите текст, который хотите закодировать";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Кодировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Результат";
+            // 
+            // inputData
+            // 
+            this.inputData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputData.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputData.Location = new System.Drawing.Point(7, 31);
+            this.inputData.Name = "inputData";
+            this.inputData.Size = new System.Drawing.Size(315, 93);
+            this.inputData.TabIndex = 5;
+            this.inputData.Text = "";
+            // 
+            // outputData
+            // 
+            this.outputData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputData.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputData.Location = new System.Drawing.Point(7, 199);
+            this.outputData.Name = "outputData";
+            this.outputData.Size = new System.Drawing.Size(315, 93);
+            this.outputData.TabIndex = 6;
+            this.outputData.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Декодировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 297);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.outputData);
+            this.Controls.Add(this.inputData);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(344, 336);
+            this.Name = "Form1";
+            this.Text = "Shifrovalshik";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox inputData;
+        private System.Windows.Forms.RichTextBox outputData;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
